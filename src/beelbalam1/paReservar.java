01515d121b60a1efa4bf69e88f1939a87faceabd
AntonioRM1231 <rodriguezantony1231@gmail.com>
2021-05-14 19:05:25 -0500

    creacion de la clase paReserva

---
+++ src/beelbalam1/paReservar.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beelbalam1;
+
+/**
+ *
+ * @author rodri
+ */
+public class paReservar {
+    private String pNombre;
+    private String sNombre;
+    private String pApellido;
+    private String sApellido;
+    private int edad;
+}
